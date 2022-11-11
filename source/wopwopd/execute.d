6eb25597995e18aae083d08b2e33faf723264335
Rob Rau <rfr2@mac.com>
2022-11-11 14:19:23 -0500

    Updated wopwop3 function to work around wopwop bug
    Updated wopwop3 function to work around a wopwop memory leak.
    We now take in the caselist object and split up the cases into batches
    of 4 so that wopwop doesn't have time to consume all available memory

--- source/wopwopd/execute.d
+++ source/wopwopd/execute.d
@@ -7,6 +7,7 @@ import std.conv;
 import std.path;
 import std.process;
 import std.stdio;
+import std.range;
 
 auto wopwop3(ref CaseList caselist, string work_dir = "./", size_t cores = 1) {
 
@@ -14,16 +15,22 @@ auto wopwop3(ref CaseList caselist, string work_dir = "./", size_t cores = 1) {
 		write_namelist(_case.namelist, work_dir~'/'~_case.globalFolderName.get()~_case.caseNameFile.get());
 	}
 
-	write_caselist(caselist, work_dir);
+	//write_caselist(caselist, work_dir);
 
-	Pid pid;
-	if(cores > 1) {
-		pid = spawnProcess(["mpirun", "-n", cores.to!string, "wopwop3"], stdin, stdout, stderr, null, Config.none, work_dir);
-	} else {
-		pid = spawnProcess("wopwop3", stdin, stdout, stderr, null, Config.none, work_dir);
+	foreach(ref cases; chunks(caselist.cases, 4)) {
+		auto sub_caselist = CaseList();
+		sub_caselist.cases = cases;
+		write_caselist(sub_caselist, work_dir);
+
+		Pid pid;
+		if(cores > 1) {
+			pid = spawnProcess(["mpirun", "-n", cores.to!string, "wopwop3"], stdin, stdout, stderr, null, Config.none, work_dir);
+		} else {
+			pid = spawnProcess("wopwop3", stdin, stdout, stderr, null, Config.none, work_dir);
+		}
+
+		wait(pid);
 	}
-
-	wait(pid);
 
 	auto results = new WopwopResult[caselist.cases.length];
 
